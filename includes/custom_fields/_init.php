42d003f78a56734803933109ce0a043b4e48a993
thefriendlydev <matt@thefriendlydev.com>
2017-03-30 03:57:38 -0400

    adding project custom fields

--- includes/custom_fields/_init.php
+++ includes/custom_fields/_init.php
@@ -4,3 +4,4 @@ include_once 'home/about.php';
 include_once 'home/my_skills.php';
 include_once 'shared/my_work.php';
 include_once 'post_types/testimonial.php';
+include_once 'post_types/project.php';
